e8f1fcbc37a3986b67e3984376555525f65b112d
khanhnguyen1202 <kitobluevip@gmail.com>
2018-09-21 13:45:59 +0700

    update

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,16 +1,47 @@
 package week1;
 
 public class Student {
-
     // TODO: khai báo các thuộc tính cho Student
-
+    private String name;
+    private String id;
+    private String group;
+    private String email;
     // TODO: khai báo các phương thức getter, setter cho Student
-
+    public String getName() {
+        return this.name;
+    }
+    public String getId() {
+        return this.id;
+    }
+    public String getGroup() {
+        return this.group;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public void setName(String _name) {
+        this.name = _name;
+    }
+    public void setId(String _id) {
+        this.id = _id;
+    }
+    public void  setGroup(String _group){
+        this.group = _group;
+    }
+    public void setEmail(String _email){
+        this.email = _email;
+    }
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        this.name = "Student";
+        this.id = "000";
+        this.group = "INT22041";
+        this.email = "uet@vnu.edu.vn";
+
+
     }
 
     /**
@@ -20,6 +51,10 @@ public class Student {
      * @param em
      */
     Student(String n, String sid, String em) {
+        this.name = n;
+        this.id = sid;
+        this.email = em;
+        this.group = "INT22041";
         // TODO:
     }
 
@@ -29,10 +64,14 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        this.name = s.name;
+        this.id = s.id;
+        this.group = s.group;
+        this.email = s.email;
     }
 
-    String getInfo() {
+    public String getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        return name + " " + id + " " + group + " " + email; // xóa dòng này sau khi cài đặt
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,23 +1,66 @@
 package week1;
-
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.*;
 public class StudentManagement {
-
-    // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
+    public static ArrayList<Student> students = new ArrayList<Student>();
     public boolean sameGroup(Student s1, Student s2) {
-        // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        if (s1==null || s2==null) return false;
+        return s1.equals(s2);
     }
 
     void studentsByGroup() {
         // TODO:
+        ArrayList<String> groupList = new ArrayList<String>();
+        for (int i = 0; i < students.size(); i++) {
+            if (!groupList.contains(students.get(i).getGroup())) {
+                groupList.add(students.get(i).getGroup());
+            }
+        }
+        for (int i = 0; i < groupList.size(); i++) {
+            System.out.println(groupList.get(i));
+            for (int j = 0; j < students.size(); j++) {
+                if (students.get(j).getGroup().equals(groupList.get(i))) {
+                    System.out.println(students.get(j).getInfo());
+                }
+            }
+        }
     }
 
     void removeStudent(String id) {
         // TODO:
+        for (int i = 0; i < students.size(); i++) {
+            if (id.equals(students.get(i).getId())) {
+                students.remove(i);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        // TODO:
+        // write your code here
+        Student s1 = new Student();
+        s1.setName("Khanh");
+        s1.setEmail("khanh@gmai.com");
+        s1.setGroup("INT22043");
+        s1.setId("002");
+        Student s2 = new Student("Minh", "001", "good@gmail.com");
+        s2.setGroup("INT22042");
+        Student s3 = new Student();
+        Student s4 = new Student(s2);
+
+        StudentManagement test = new StudentManagement();
+        test.students.add(s1);
+        test.students.add(s2);
+        test.students.add(s3);
+        test.students.add(s4);
+        test.studentsByGroup();
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i).getInfo());
+        }
+        System.out.println(test.sameGroup(students.get(1),students.get(2)));
+        test.removeStudent("001");
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i).getInfo());
+        }
+
     }
 }
